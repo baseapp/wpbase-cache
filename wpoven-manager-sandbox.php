83906dc91407169629639f2dc39e146f1fceba28
Tarun Bansal <noushter@gmail.com>
2013-06-03 13:12:06 +0530

    minor bugfinx in sandbox

--- wpoven-manager-sandbox.php
+++ wpoven-manager-sandbox.php
@@ -6,16 +6,18 @@ class WPOven_Sandbox {
     public $domain;
 
     public function __construct() {
-        $this->host_name = $_SERVER["SERVER_NAME"];
-        $this->host_port = $_SERVER["SERVER_PORT"];
-        $this->domain = $_SERVER["REQUEST_URI"];
-        $this->domain = explode("/", $this->domain);
-        $this->domain = $this->domain[1];
+        if(isset($_SERVER['SERVER_SOFTWARE']) && !(php_sapi_name() == 'cli' || (is_numeric($_SERVER['argc']) && $_SERVER['argc'] > 0))) {
+            $this->host_name = $_SERVER["SERVER_NAME"];
+            $this->host_port = $_SERVER["SERVER_PORT"];
+            $this->domain = $_SERVER["REQUEST_URI"];
+            $this->domain = explode("/", $this->domain);
+            $this->domain = $this->domain[1];
 
-        if(filter_var($this->host_name, FILTER_VALIDATE_IP)){
-            define('WPOVEN_MANAGER_SANDBOX', true);
-            add_filter('pre_option_home', array($this, 'sandbox'));
-            add_filter('pre_option_siteurl', array($this, 'sandbox'));
+            if(filter_var($this->host_name, FILTER_VALIDATE_IP)){
+                define('WPOVEN_MANAGER_SANDBOX', true);
+                add_filter('pre_option_home', array($this, 'sandbox'));
+                add_filter('pre_option_siteurl', array($this, 'sandbox'));
+            }
         }
     }
 
