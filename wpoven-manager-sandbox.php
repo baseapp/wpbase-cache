bdf1e2245d2c1d09ca4c936ed4eb9616c80cae4e
Tarun Bansal <noushter@gmail.com>
2013-06-11 13:32:10 +0530

    added automatic apc cache flush for sandbox

--- wpoven-manager-sandbox.php
+++ wpoven-manager-sandbox.php
@@ -17,6 +17,24 @@ class WPOven_Sandbox {
                 define('WPOVEN_MANAGER_SANDBOX', true);
                 add_filter('pre_option_home', array($this, 'sandbox'));
                 add_filter('pre_option_siteurl', array($this, 'sandbox'));
+
+                // flush automatic apc cache if in sandbox
+                add_action('switch_theme', array($this, 'apc_cache_flush'));
+                add_action('publish_phone', array($this, 'apc_cache_flush'));
+                add_action('publish_post', array($this, 'apc_cache_flush'));
+                add_action('edit_post', array($this, 'apc_cache_flush'));
+                add_action('save_post', array($this, 'apc_cache_flush'));
+                add_action('wp_trash_post', array($this, 'apc_cache_flush'));
+                add_action('delete_post', array($this, 'apc_cache_flush'));
+                add_action('trackback_post', array($this, 'apc_cache_flush'));
+                add_action('pingback_postt', array($this, 'apc_cache_flush'));
+                add_action('comment_post', array($this, 'apc_cache_flush'));
+                add_action('edit_comment', array($this, 'apc_cache_flush'));
+                add_action('wp_set_comment_status', array($this, 'apc_cache_flush'));
+                add_action('delete_comment', array($this, 'apc_cache_flush'));
+                add_action('comment_cookie_lifetime', array($this, 'apc_cache_flush'));
+                add_action('wp_update_nav_menu', array($this, 'apc_cache_flush'));
+                add_action('edit_user_profile_update', array($this, 'apc_cache_flush'));
             }
         }
     }
@@ -24,6 +42,14 @@ class WPOven_Sandbox {
     public function sandbox() {
         $url = 'http://'.$this->host_name.':'.$this->host_port.'/'.$this->domain;
         return $url;
+    }
+
+    public function apc_cache_flush() {
+        if ( function_exists( 'apc_clear_cache' ) ) {
+            apc_clear_cache();
+            apc_clear_cache('user');
+            apc_clear_cache('opcode');
+        }
     }
 }
 
